5c8f8d082de6f155dd388a1a66c14a0ea8851b5e
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-17 14:39:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names = ["Guadalupe","Ollie","Aki"], event = "surprise") {
+    const thankyoumessage = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankyoumessage.push(message);
+    } 
+    return thankyoumessage;
+  }
+
+function countDown(num = 10) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
